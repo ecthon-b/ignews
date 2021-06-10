e7c1c7d4de9af75d6e59501d823ddb5af8a59bd9
Ecthon Borhis <ecthon@gmail.com>
2021-06-09 21:28:09 -0400

    Checking for duplicate user

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -25,10 +25,26 @@ export default NextAuth({
             console.log(user)
             try {
                 await fauna.query(
-                    q.Create(
-                        q.Collection('users'),
-                        { data: { email } }
-                    )
+                    q.If (
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'),
+                                    q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('user'),
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'),
+                                q.Casefold(user.email)
+                            )
+                        )
+                    )       
                 )
                 return true
             } catch {
