493d1da2392d874d912590b65ee6cc34673fc23e
Ecthon Borhis <ecthon@gmail.com>
2021-06-10 09:41:58 -0400

    Generating checkout session

--- src/components/SubscribButton/index.tsx
+++ src/components/SubscribButton/index.tsx
@@ -1,14 +1,27 @@
+import { useSession, signIn } from 'next-auth/client';
 import styles from './styles.module.scss';
 
 interface SubscribeButtonProps {
     priceId: string;
 }
 
-export function SubscribButton({ priceId }: SubscribeButtonProps) {
+export function SubscribButton({ priceId }: SubscribeButtonProps) {]
+    const [session] = useSession();
+
+    function handleSubscribe() {
+        if(!session) {
+            signIn('github')
+            return;
+        }
+
+        // criação da checkout session
+        
+    }
     return (
         <button
             type="button"
-            className={styles.subscribButton}
+            className={ styles.subscribButton }
+            onClick={ handleSubscribe }
         >
             Subscribe now
         </button>
